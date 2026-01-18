4be1d53f5808bb2c4166e3fcccc75ddc3d967a5f
Amirjon Ulmasov <amir@Amirjons-MacBook-Air.local>
2026-01-18 01:49:14 -0500

    fixed bug

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,7 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  basePath: '/interviewer',
-  assetPrefix: '/interviewer',
+  // nothing here
 };
 
 export default nextConfig;
